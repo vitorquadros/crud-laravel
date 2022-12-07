63105a4c1f43e0447864683cdbbf1300f1bab26c
Vitor Quadros <viitorquadros@hotmail.com>
2022-12-07 10:06:28 -0300

    create livewire component

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('appointments')" :active="request()->routeIs('dashboard/appointments')">
+                        {{ __('Appointments') }}
+                    </x-nav-link>
                 </div>
             </div>
 
